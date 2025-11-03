e65e15b374c3bd429414c83ee7e21d9749541a20
kyungjaee <kkyungjaee@gmail.com>
2025-11-03 14:03:20 -0500

    added user routes

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=> {
     console.log('here');
-    res.render("index");
+    res.render("index", {user: "Josh"});
 }); // this function will run when someone goes to the root folder
-
+app.get('/users', (req, res)=> {
+    res.send('user list')
+});
+app.get('/users/new', (req, res)=> {
+    res.send('new user form')
+});
 app.listen(3030);
 
