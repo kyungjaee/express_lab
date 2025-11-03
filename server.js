5a8cee119759e8e5c0760476a9b92c7f0f36512b
kyungjaee <kkyungjaee@gmail.com>
2025-11-03 13:40:24 -0500

    added ejs and created first html page

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const app = express(); //calling this function sets up a server
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=> {
     console.log('here');
-    res.send('hello world');
+    res.render("index");
 }); // this function will run when someone goes to the root folder
 
 app.listen(3030);
