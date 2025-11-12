7b81b989cb7bf336959711b04ad354e985657909
kyungjaee <kkyungjaee@gmail.com>
2025-11-12 13:47:08 -0500

    made id route

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,18 @@ router.get('/', (req, res)=> {
 router.get('/new', (req, res)=> {
     res.send('new user form')
 });
+router.post('/', (req, res) => {
+
+});
+// router.get('/:id', (req,res)=> {
+    //res.send('Getting user data: ${req.parmas.id}');
+//});
+router.route("/:id").get('/:id',(req,res)=> {
+    res.send('Getting user data: ${req.params.id}');
+}).delete((req, res)=> {
+    res.send('Deleting user with id: ${req.params.id}');
+}).put((req,res)=>{ 
+    res.send('updating user with id: ${req.params.id}');
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const userRouter = require('./routes/users');
 const postRouter = require('./routes/posts');
+
+
 const app = express(); //calling this function sets up a server
 app.set('view engine', 'ejs');
 app.use('/post', newPost);
-
+app.use('/users, userRouter')
 app.get('/', (req, res)=> {
     console.log('here');
     res.render("index", {user: "Josh"});
 }); // this function will run when someone goes to the root folder
-app.get('/post', (req, res)=> {
-    res.send('posts')
+app.get('/', (req, res)=> {
+    res.render('index', {user: "Josh",});
+
 });
-app.get('/post/new', (req, res)=> {
-    res.send('new post form')
-});
+
+
 app.listen(3030);
 
 
